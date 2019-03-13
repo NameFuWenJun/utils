b8a136a5d8b65df7e269afb6188a5c66137c7a47
fuwenjun01 <fuwenjun01@jmenv.tbsite.net>
2019-03-13 10:41:03 +0800

    优化方法

--- projectUtils/src/main/java/com/fuwenjun/projectUtils/jdbc/SQLUtils.java
+++ projectUtils/src/main/java/com/fuwenjun/projectUtils/jdbc/SQLUtils.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.swing.plaf.basic.BasicTreeUI.CellEditorHandler;
+
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.MapHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
@@ -26,6 +28,13 @@ import net.sf.json.JSONObject;
 public class SQLUtils {
     private Connection connection;
     private DruidDataSource druidDataSource;
+
+    //对每个连接在sqlUtils中进行保存
+    private DataBase type;
+    private String ipAndPort;
+    private String database;
+    private String userName;
+    private String password;
     /**
      * @param type:数据库种类,目前有MYSQL及ORACLE 2类
      * @param url :数据库连接地址
@@ -34,95 +43,100 @@ public class SQLUtils {
      * @throws Exception 
      */
     public SQLUtils(DataBase type,String ipAndPort,String database,String userName,String password) throws Exception {
-      init(type, ipAndPort, database,userName, password);
+        this.type = type;
+        this.ipAndPort = ipAndPort;
+        this.database = database;
+        this.userName = userName;
+        this.password = password;
+        init();
     }
-    private void init(DataBase type,String ipAndPort,String database,String userName,String password) throws Exception{
-       // DataSource dataSource=new DataSource(url, userName, password); 10.11,shine,注释
-            //connection=dataSource.getConnect(type); 10.11,shine,注释
-           // druidDataSource=dataSource.getDataSource(); 10.11,shine,注释
-        	Map<String,Object> info = new HashMap<String,Object>();
-    		info.put(DatabaseInfoConstants.DATATYPE, type);
-    		info.put(DatabaseInfoConstants.IPANDPORT, ipAndPort);
-    		info.put(DatabaseInfoConstants.DATABASE, database);
-    		info.put(DatabaseInfoConstants.USERNAME, userName);
-    		info.put(DatabaseInfoConstants.PASSWORD, password);
-    		druidDataSource = DatasourceUtil.getDataSource(info);
-    		connection = druidDataSource.getConnection();
+    private void init() throws Exception{
+        // DataSource dataSource=new DataSource(url, userName, password); 10.11,shine,注释
+        //connection=dataSource.getConnect(type); 10.11,shine,注释
+        // druidDataSource=dataSource.getDataSource(); 10.11,shine,注释
+        Map<String,Object> info = new HashMap<String,Object>();
+        info.put(DatabaseInfoConstants.DATATYPE, type);
+        info.put(DatabaseInfoConstants.IPANDPORT, ipAndPort);
+        info.put(DatabaseInfoConstants.DATABASE, database);
+        info.put(DatabaseInfoConstants.USERNAME, userName);
+        info.put(DatabaseInfoConstants.PASSWORD, password);
+        druidDataSource = DatasourceUtil.getDataSource(info);
+        connection = druidDataSource.getConnection();
+    }
+    /**
+     * 判断DataSource有没有被关闭
+     * @throws Exception 
+     */
+    private void cheackDataSource() throws Exception{
+        //数据连接池如果被关闭
+        if(druidDataSource.isClosed()){
+            //重新初始化DataSource
+            init();
+        }
     }
     /**
      * 获取数据库连接
      * @return
+     * @throws Exception 
      */
-    public Connection getConnection() {
-        try {
-            if(connection.isClosed()){
-                connection=druidDataSource.getConnection();
-            }
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public Connection getConnection() throws Exception {
+        cheackDataSource();
+        if(connection.isClosed()){
+            connection=druidDataSource.getConnection();
         }
         return connection;
     }
-    
+
     /**
      * 查询一组数据
      * @param sql  :查询语句
      * @return
+     * @throws Exception 
      */
-    public List<Map<String, Object>> selectWithList(String sql){
+    public List<Map<String, Object>> selectWithList(String sql) throws Exception{
+        cheackDataSource();
         List<Map<String, Object>>resultMap=new ArrayList<Map<String, Object>>();
         QueryRunner queryRunner=new QueryRunner(druidDataSource);
-        try {
-            resultMap=queryRunner.query(sql, new MapListHandler());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        resultMap=queryRunner.query(sql, new MapListHandler());
         return resultMap;
     }
     /**
      * 查询一组数据带有参数
      * @param sql  :查询语句,参数放入condition数组中
      * @return
+     * @throws Exception 
      */
-    public List<Map<String, Object>> selectWithListAndCondition(String sql,String [] conditions){
+    public List<Map<String, Object>> selectWithListAndCondition(String sql,String [] conditions) throws Exception{
+        cheackDataSource();
         List<Map<String, Object>>resultMap=new ArrayList<Map<String, Object>>();
         QueryRunner queryRunner=new QueryRunner(druidDataSource);
-        try {
-            resultMap=queryRunner.query(sql,conditions,new MapListHandler());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        resultMap=queryRunner.query(sql,conditions,new MapListHandler());
         return resultMap;
     }
     /**
      * 查询单条数据
      * @param sql
      * @return
+     * @throws Exception 
      */
-    public Map<String, Object> selectWithMap(String sql){
+    public Map<String, Object> selectWithMap(String sql) throws Exception{
+        cheackDataSource();
         Map<String, Object>resultMap=new HashMap<String, Object>();
         QueryRunner queryRunner=new QueryRunner(druidDataSource);
-        try {
-            resultMap=queryRunner.query(sql, new MapHandler());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        resultMap=queryRunner.query(sql, new MapHandler());
         return resultMap;
     }
     /**
      * 查询单条数据带有参数
      * @param sql,参数放入数组之中
      * @return
+     * @throws Exception 
      */
-    public Map<String, Object> selectWithMapAndCondition(String sql,String [] conditions){
+    public Map<String, Object> selectWithMapAndCondition(String sql,String [] conditions) throws Exception{
+        cheackDataSource();
         Map<String, Object>resultMap=new HashMap<String, Object>();
         QueryRunner queryRunner=new QueryRunner(druidDataSource);
-        try {
-            resultMap=queryRunner.query(sql, conditions,new MapHandler());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        resultMap=queryRunner.query(sql, conditions,new MapHandler());
         return resultMap;
     }
     /**
@@ -130,19 +144,16 @@ public class SQLUtils {
      * @param sql
      * @param conditions
      * @return
+     * @throws Exception 
      */
-    public int update(String sql,Object [] conditions){
+    public int update(String sql,Object [] conditions) throws Exception{
+        cheackDataSource();
         QueryRunner queryRunner=new QueryRunner(druidDataSource);
-        int result=0;
-        try {
-            result=queryRunner.update(sql, conditions);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        int result=queryRunner.update(sql, conditions);
         return result;
     }
-    
-    
+
+
     /**
      * 含有事务的插入语句
      * @param jsonArray  jsonArray为一个Json数组
